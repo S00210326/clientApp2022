49a2f3ef95814ac976ba901868e759421138a6f2
paulmcgonigle67 <s00210326@atu.ie>
2022-11-14 17:33:50 +0000

    Working with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { BookListComponent } from './book/book-list/book-list.component';
+import { BookRowComponent } from './book/book-row/book-row.component';
+import { BookDetailsComponent } from './book/book-details/book-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    BookListComponent,
+    BookRowComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
